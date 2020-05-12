b4448d054ee68f464e9df111be88ad90877f8ec8
ArtHome12 <_mag12@yahoo.com>
2020-05-12 20:07:02 +0700

    Fix INA addr

--- ha_arduino_sh.ino
+++ ha_arduino_sh.ino
@@ -14,7 +14,7 @@ Copyright (c) 2019 by Artem Khomenko _mag12@yahoo.com.
 
 const uint16_t ina226calib = INA226_asukiaaa::calcCalibByResisterMilliOhm(100); // Max 5120 milli ohm
 #define INA226_ASUKIAAA_MAXAVERAGE_CONFIG 0x4F27                                // Default 0x4127 - for once average. Digit F for 1024 averages
-INA226_asukiaaa voltCurrMeter(INA226_ASUKIAAA_ADDR_A0_VDD_A1_VDD, ina226calib, INA226_ASUKIAAA_MAXAVERAGE_CONFIG);
+INA226_asukiaaa voltCurrMeter(INA226_ASUKIAAA_ADDR_A0_VDD_A1_GND, ina226calib, INA226_ASUKIAAA_MAXAVERAGE_CONFIG);
 
 HTU21D myHTU21D(HTU21D_RES_RH12_TEMP14);// Интерфейс к датчикам температуры и влажности.
 
@@ -309,7 +309,7 @@ void powerControl(int voltage, int power){
   }
   
   // 5. Проверяем на предельное время работы без перезагрузки.
-  if (previousMillis > maxWorkTimeCurLimit) {
+  /*if (previousMillis > maxWorkTimeCurLimit) {
     // Посылаем сигнал завершения работы малины, если ещё не сделано.
     sendShutdown();
 
@@ -318,7 +318,7 @@ void powerControl(int voltage, int power){
 
     // Выходим, иначе проверка на достаточное напряжение аннулирует выключение.
     return;
-  }
+  }*/
   
   
 
